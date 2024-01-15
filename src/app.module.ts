87fb98d497ec5123d2b533cee4d68a67107c7549
Hovhannes Hovhannisyan <hhovhannisyan@aramaze.com>
2024-01-15 19:32:29 +0400

    feat: Implement Auth and User modules with JWT and bcrypt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database/app.db',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+console.log(join(__dirname, '**', '*.entity.{ts,js}'));
