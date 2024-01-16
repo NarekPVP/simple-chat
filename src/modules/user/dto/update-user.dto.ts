56f773ba055cb097a370ec01a2da8e81c0153400
Hovhannes Hovhannisyan <hhovhannisyan@aramaze.com>
2024-01-16 16:43:45 +0400

    feat:Improve Swagger documentation for SignUpDto with descriptions and examples

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,6 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    required: false,
+    description: 'The refresh token of the user',
+    example: 'updated-refresh-token',
+  })
+  @IsString()
+  @IsOptional()
   refreshToken: string;
 }
