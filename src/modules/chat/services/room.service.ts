8d0866e9d79b4824e615d11a715ab2f68df46022
Hovhannes Hovhannisyan <hhovhannisyan@aramaze.com>
2024-01-26 17:42:44 +0400

    feat(room-update): Enhance room updating functionality
    - Streamline participant handling by automatically including the updater in the participants list if not already included.
    - Optimize error handling by utilizing service layer exceptions for room existence validation.
    - Implement notification logic to inform all room participants of updates in real-time.
    - Improve code readability and maintainability in the onUpdateRoom method.
    
    This update ensures a smoother user experience by providing immediate feedback to room participants upon any room updates and simplifies the backend logic for maintaining room state.

--- src/modules/chat/services/room.service.ts
+++ src/modules/chat/services/room.service.ts
@@ -84,22 +84,40 @@ export class RoomService {
     }
   }
 
-  async update(id: string, updateRoomDto: UpdateRoomDto): Promise<Room> {
+  async update(
+    userId: string,
+    roomId: string,
+    updateRoomData: UpdateRoomDto,
+    participants: User[],
+  ): Promise<Room> {
     try {
-      const room = await this.roomRepository.preload({
-        id,
-        ...updateRoomDto,
+      const roomToUpdate = await this.roomRepository.preload({
+        id: roomId,
+        ...updateRoomData,
+        participants,
+        updatedBy: userId,
+        updatedAt: new Date(),
       });
-      if (!room) {
-        throw new WsException(`Room with ID "${id}" not found`);
+
+      if (!roomToUpdate) {
+        this.logger.warn(
+          `Attempted to update a non-existent room with ID: ${roomId}`,
+        );
+        throw new WsException(`Room with ID "${roomId}" not found.`);
       }
-      return await this.roomRepository.save(room);
+
+      const updatedRoom = await this.roomRepository.save(roomToUpdate);
+
+      this.logger.log(`Room with ID ${roomId} updated successfully.`);
+      return updatedRoom;
     } catch (error) {
       this.logger.error(
-        `Failed to update room with ID ${id}: ${error.message}`,
+        `Failed to update room with ID ${roomId}: ${error.message}`,
         error.stack,
       );
-      throw new WsException('Error occurred while updating the room.');
+      throw new WsException(
+        'An error occurred while updating the room. Please try again.',
+      );
     }
   }
 
